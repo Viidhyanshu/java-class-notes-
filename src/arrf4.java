38f96626265cb8ab467e658b74ef24bf9642e831
Vidhyanshu Kumar <vidhyanshukumarvidhyam@gmail.com>
2026-08-20 01:08:38 +0530

    scanner function, 18 aug

--- src/arrf4.java
+++ src/arrf4.java
@@ -1,12 +1,14 @@
+import java.util.*;
 class student{
-    int name;
+    String name;
     int marks;
     int att;
     int reg;
     String city;
     static int count = 250;
 
-    student(int marks, String city){
+    student( String name, String city, int marks){
+        this.name = name;
         this.marks = marks;
         this.city = city;
         reg = count++;
@@ -20,13 +22,31 @@ class student{
         System.out.println("city of student" + city);
         System.out.println("att of student" + att);
     }
-    void findcity(){
-
+    static void findcity(student s[]){
+        for(int i =0 ; i < s.length; i++){
+            if (s[i] != null && "Jaipur".equalsIgnoreCase(s[i].city)){
+                System.out.println("Student from Jaipur"+ s[i].name);
+            }
+        }
     }
+    
 }
 class arrf4 {
     public static void main(String[] args) {
-        student s[] = new student[5];
+        student s[] = new student[3];
+        Scanner sc = new Scanner (System.in);
+
+        for (int i = 0; i < s.length; i++) {
+            System.out.println("enter name");
+            String name = sc.next();
+            System.out.println("enter city");
+            String city = sc.next();
+            System.out.println("enter marks");
+            int marks = sc.nextInt();
+            s[i]= new student(null, null, i);
+
+        }
+        student.findcity(s);
 
     }
 }
